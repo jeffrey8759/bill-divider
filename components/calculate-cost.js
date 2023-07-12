71765343ef1c2d86145ba38decedeff8b04a969b
Jeffrey Cheng <chengjeffrey8759@gmail.com>
2023-07-12 02:14:38 -0700

    Multi item functionality

--- components/calculate-cost.js
+++ components/calculate-cost.js
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import styles from './calculate-cost.module.css'
 
+let nextIdIndividual = 0;
+let nextIdCommunal = 0;
 export default function Form() {
-    const [subtotal , setSubtotal] = useState(null);
-    const [tax, setTax] = useState(null);
-    const [tip, setTip] = useState(null);
-    const [individualCost, setIndividualCost] = useState(null);
+    const [subtotal , setSubtotal] = useState('');
+    const [tax, setTax] = useState('');
+    const [tip, setTip] = useState('');
+    const [participant, setParticipant] = useState(1);
+    const [IndividualItemCost, setIndividualItemCost] = useState('');
+    const [IndividualItemList, setIndividualItemList] = useState([]);
+    const [communalItemCost, setCommunalItemCost] = useState('');
+    const [communalItemList, setCommunalItemList] = useState([]);
 
     function setDefaultTaxTip(subtotal) {
         setTax(parseFloat(subtotal * .101).toFixed(2));
@@ -13,11 +19,11 @@ export default function Form() {
     }
 
     return (
-        <form className={styles.calculationForm}>
+        <form className={styles.calculationForm} action="http://venmo.com">
             <div className={styles.formDiv}>
-                <label className={styles.formLabel}> Subtotal: </label>
+            <label className={styles.formLabel}> Subtotal: </label>
                 <span>$ </span>
-                <input className={styles.formInput} type = "number" inputmode="decimal" value = {subtotal} onChange = {e => {
+                <input className={styles.formInput} type = "text" inputMode="decimal" value = {subtotal} onChange = {e => {
                     setSubtotal(e.target.value);
                     setDefaultTaxTip(e.target.value);
                 }} />
@@ -25,32 +31,117 @@ export default function Form() {
             <div className={styles.formDiv}>
                 <label className={styles.formLabel}> Tax (default 10.1%): </label>
                 <span>$ </span>
-                <input className={styles.formInput} type = "number" inputmode="decimal" defaultValue = {tax} value = {tax} onChange = {e => setTax(e.target.value)} />
+                <input className={styles.formInput} type = "text" inputMode="decimal" value = {tax} onChange = {e => setTax(e.target.value)} />
             </div>
             <div className={styles.formDiv}>
                 <label className={styles.formLabel}> Tip (default 15%): </label>
                 <span>$ </span>
-                <input className={styles.formInput} type = "number" inputmode="decimal" defaultValue = {tip} value = {tip} onChange = {e => setTip(e.target.value)} />
+                <input className={styles.formInput} type = "text" inputMode="decimal" value = {tip} onChange = {e => setTip(e.target.value)} />
             </div>
-            <div className={styles.formDiv} id='itemList'>
-                <label className={styles.formLabel}> Individual Cost: </label>
+            <div className={styles.formDiv} id='individualItemInput'>
+                <label className={styles.formLabel}> Individual Item Cost: </label>
                 <span>$ </span>
-                <input className={styles.formInput} type = "number" inputmode="decimal" value = {individualCost} onChange = {e => setIndividualCost(e.target.value)} />
+                <input
+                    className={styles.formInput}
+                    type = "text"
+                    inputMode="decimal"
+                    value={IndividualItemCost}
+                    onChange={e => setIndividualItemCost(e.target.value)}
+                />
+                <button className={styles.addItemButton} type="button" onClick={() => {
+                    setIndividualItemList([
+                    ...IndividualItemList,
+                    { id: nextIdIndividual++, itemCost: IndividualItemCost }
+                    ]);
+                    setIndividualItemCost("");
+                }}>Add</button>
+                
+            </div>
+            <div className={styles.formDiv} id='individualItemList'>
+                <label>Individual Item List:</label>
+                <ul>
+                    {IndividualItemList.map(item => (
+                        <li key={item.id}>
+                            $ {item.itemCost}{' '}
+                            <button onClick={() => {
+                                setIndividualItemList(
+                                    IndividualItemList.filter(a =>
+                                    a.id !== item.id
+                                    )
+                                );
+                            }}>
+                                Delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className={styles.formDiv} id='communalParticipantInput'>
+                <label className={styles.formLabel}> # of Participants: </label>
+                <input
+                    className={styles.participantInput}
+                    type = "text"
+                    inputMode="numeric"
+                    value={participant}
+                    onChange={e => setParticipant(e.target.value)}
+                />
+            </div>
+            <div className={styles.formDiv} id='communalItemInput'>
+                <label className={styles.formLabel}> Communal Item Cost: </label>
+                <span>$ </span>
+                <input
+                    className={styles.formInput}
+                    type = "text"
+                    inputMode="decimal"
+                    value={communalItemCost}
+                    onChange={e => setCommunalItemCost(e.target.value)}
+                />
+                <button className={styles.addItemButton} type="button" onClick={() => {
+                    setCommunalItemList([
+                    ...communalItemList,
+                    { id: nextIdCommunal++, communalItemCost: communalItemCost }
+                    ]);
+                    setCommunalItemCost("");
+                }}>Add</button>
+                
+            </div>
+            <div className={styles.formDiv} id='communalItemList'>
+                <label>Communal Item List:</label>
+                <ul>
+                    {communalItemList.map(item => (
+                        <li key={item.id}>
+                            $ {item.communalItemCost}{' '}
+                            <button onClick={() => {
+                                setCommunalItemList(
+                                    communalItemList.filter(a =>
+                                    a.id !== item.id
+                                    )
+                                );
+                            }}>
+                                Delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
             </div>
             <div className={styles.formDiv}>
                 <label className={styles.formLabel}> Amount Owed: </label>
-                $ {calculateAmount(parseInt(subtotal), parseInt(tax), parseInt(tip), parseInt(individualCost))}
+                $ {calculateAmount(parseFloat(subtotal), parseFloat(tax), parseFloat(tip), parseFloat(participant), IndividualItemList, communalItemList)}
             </div>
+            <input type="submit" value="Go to Venmo" />
         </form>
     );
 }
 
-function handleAddItem(){
-
-}
-
-function calculateAmount(subtotal, tax, tip, individualCost) {
-    return parseFloat(individualCost + (individualCost / subtotal) * (tax + tip)).toFixed(2);
+function calculateAmount(subtotal, tax, tip, participant, individualItemList, communalItemList) {
+    let individualItemCostSum = individualItemList.reduce((partialSum, a) => partialSum + parseFloat(a.itemCost), 0);
+    let communalItemCostSum = communalItemList.reduce((partialSum, a) => partialSum + parseFloat(a.communalItemCost), 0);
+    let result = parseFloat(individualItemCostSum + communalItemCostSum / participant + (tax + tip) * ((individualItemCostSum + communalItemCostSum / participant) / subtotal));
+    if (isNaN(result)){
+        return 0;
+    } else {
+        return result;
+    } 
 }
 
 
