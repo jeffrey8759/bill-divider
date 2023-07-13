493d940e00c6c3133177873b0c139ceada5878d3
Jeffrey Cheng <chengjeffrey8759@gmail.com>
2023-07-12 20:22:08 -0700

    make form fit phone screen width

--- components/calculate-cost.js
+++ components/calculate-cost.js
@@ -7,7 +7,7 @@ export default function Form() {
     const [subtotal , setSubtotal] = useState('');
     const [tax, setTax] = useState('');
     const [tip, setTip] = useState('');
-    const [participant, setParticipant] = useState(1);
+    const [participant, setParticipant] = useState(2);
     const [IndividualItemCost, setIndividualItemCost] = useState('');
     const [IndividualItemList, setIndividualItemList] = useState([]);
     const [communalItemCost, setCommunalItemCost] = useState('');
@@ -22,48 +22,56 @@ export default function Form() {
     return (
         <form className={styles.calculationForm} action="venmo://paycharge">
             <h1 className={styles.header}>Bill Divider</h1>
-            <div className={styles.formSection} id='billSummary'>
-                <div className={styles.formDiv}>
-                    <label className={styles.formLabel}> Subtotal: </label>
+            <div className={styles.inputSection} id='billSummary'>
+                <div className={styles.inputDiv}>
+                    <label className={styles.label}> Subtotal: </label>
+                        <div className={styles.inputContainer}>
+                            <span>$ </span>
+                            <input className={styles.input} type = "text" inputMode="decimal" value = {subtotal} onChange = {e => {
+                                setSubtotal(e.target.value);
+                                setDefaultTaxTip(e.target.value);
+                            }} />
+                        </div>
+                </div>
+                <div className={styles.inputDiv}>
+                    <label className={styles.label}> Tax (default 10.1%): </label>
+                    <div className={styles.inputContainer}>
                         <span>$ </span>
-                        <input className={styles.formInput} type = "text" inputMode="decimal" value = {subtotal} onChange = {e => {
-                            setSubtotal(e.target.value);
-                            setDefaultTaxTip(e.target.value);
-                        }} />
+                        <input className={styles.input} type = "text" inputMode="decimal" value = {tax} onChange = {e => setTax(e.target.value)} />
+                    </div>
                 </div>
-                <div className={styles.formDiv}>
-                    <label className={styles.formLabel}> Tax (default 10.1%): </label>
-                    <span>$ </span>
-                    <input className={styles.formInput} type = "text" inputMode="decimal" value = {tax} onChange = {e => setTax(e.target.value)} />
-                </div>
-                <div className={styles.formDiv}>
-                    <label className={styles.formLabel}> Tip (default 15%): </label>
-                    <span>$ </span>
-                    <input className={styles.formInput} type = "text" inputMode="decimal" value = {tip} onChange = {e => setTip(e.target.value)} />
+                <div className={styles.inputDiv}>
+                    <label className={styles.label}> Tip (default 15%): </label>
+                    <div className={styles.inputContainer}>
+                        <span>$ </span>
+                        <input className={styles.input} type = "text" inputMode="decimal" value = {tip} onChange = {e => setTip(e.target.value)} />
+                    </div>
                 </div>
             </div>
             
-            <div className={styles.formSection} id='individualItem'>
-                <div className={styles.formDiv} id='individualItemInput'>
-                    <label className={styles.formLabel}> Individual Item Cost: </label>
-                    <span>$ </span>
-                    <input
-                        className={styles.formInput}
-                        type = "text"
-                        inputMode="decimal"
-                        value={IndividualItemCost}
-                        onChange={e => setIndividualItemCost(e.target.value)}
-                    />
-                    <button className={styles.addItemButton} type="button" onClick={() => {
-                        setIndividualItemList([
-                        ...IndividualItemList,
-                        { id: nextIdIndividual++, itemCost: IndividualItemCost }
-                        ]);
-                        setIndividualItemCost("");
-                    }}>Add</button>
-                    
+            <div className={styles.inputSection} id='individualItem'>
+                <div className={styles.inputDiv} id='individualItemInput'>
+                    <label className={styles.label}> Individual Item Cost: </label>
+                    <div className={styles.inputContainer}>
+                        <span>$ </span>
+                        <input
+                            className={styles.addItemInput}
+                            type = "text"
+                            inputMode="decimal"
+                            value={IndividualItemCost}
+                            onChange={e => setIndividualItemCost(e.target.value)}
+                        >
+                        </input>
+                        <button className={styles.addItemButton} type="button" onClick={() => {
+                            setIndividualItemList([
+                            ...IndividualItemList,
+                            { id: nextIdIndividual++, itemCost: IndividualItemCost }
+                            ]);
+                            setIndividualItemCost("");
+                        }}>Add</button>
+                    </div>
                 </div>
-                <div className={styles.formList} id='individualItemList'>
+                <div className={styles.itemList} id='individualItemList'>
                     <label>Individual Item List:</label>
                     <ul>
                         {IndividualItemList.map(item => (
@@ -85,44 +93,48 @@ export default function Form() {
             </div>
 
             <br/>
-            <button type="button" onClick={() => {
+            <button className={styles.formButton} type="button" onClick={() => {
                 setShowCommunalItemSection(!showCommunalItemSection);
             }}
                 >{ showCommunalItemSection ? "Hide Communal Items" : "Show Communal Items" }
             </button>
 
             { showCommunalItemSection ?
-                <div className={styles.formSection} id='communalItem'>
-                    <div className={styles.formDiv} id='communalParticipantInput'>
-                        <label className={styles.formLabel}> # of Participants: </label>
-                        <input
-                            className={styles.participantInput}
-                            type = "text"
-                            inputMode="numeric"
-                            value={participant}
-                            onChange={e => setParticipant(e.target.value)}
-                        />
+                <div className={styles.inputSection} id='communalItem'>
+                    <div className={styles.inputDiv} id='communalParticipantInput'>
+                        <label className={styles.label}> # of Participants: </label>
+                        <div className={styles.inputContainer}>
+                            <span>&nbsp;&nbsp; </span>
+                            <input
+                                className={styles.input}
+                                type = "text"
+                                inputMode="numeric"
+                                value={participant}
+                                onChange={e => setParticipant(e.target.value)}
+                            />
+                        </div>
                     </div>
-                    <div className={styles.formDiv} id='communalItemInput'>
-                        <label className={styles.formLabel}> Communal Item Cost: </label>
-                        <span>$ </span>
-                        <input
-                            className={styles.formInput}
-                            type = "text"
-                            inputMode="decimal"
-                            value={communalItemCost}
-                            onChange={e => setCommunalItemCost(e.target.value)}
-                        />
-                        <button className={styles.addItemButton} type="button" onClick={() => {
-                            setCommunalItemList([
-                            ...communalItemList,
-                            { id: nextIdCommunal++, communalItemCost: communalItemCost }
-                            ]);
-                            setCommunalItemCost("");
-                        }}>Add</button>
-                        
+                    <div className={styles.inputDiv} id='communalItemInput'>
+                        <label className={styles.label}> Communal Item Cost: </label>
+                        <div className={styles.inputContainer}>
+                            <span>$ </span>
+                            <input
+                                className={styles.addItemInput}
+                                type = "text"
+                                inputMode="decimal"
+                                value={communalItemCost}
+                                onChange={e => setCommunalItemCost(e.target.value)}
+                            />
+                            <button className={styles.addItemButton} type="button" onClick={() => {
+                                setCommunalItemList([
+                                ...communalItemList,
+                                { id: nextIdCommunal++, communalItemCost: communalItemCost }
+                                ]);
+                                setCommunalItemCost("");
+                            }}>Add</button>
+                        </div>
                     </div>
-                    <div className={styles.formList} id='communalItemList'>
+                    <div className={styles.itemList} id='communalItemList'>
                         <label>Communal Item List:</label>
                         <ul>
                             {communalItemList.map(item => (
@@ -143,13 +155,15 @@ export default function Form() {
                     </div>
                 </div>
             : null }
-            
-            <div className={styles.formDiv}>
-                <label className={styles.formLabel}> Amount Owed: </label>
+
+            <br/>
+            <div className={styles.inputDiv}>
+                <label className={styles.label}> Amount Owed: </label>
                 $ {calculateAmount(parseFloat(subtotal), parseFloat(tax), parseFloat(tip), parseFloat(participant), IndividualItemList, communalItemList).toFixed(2)}
             </div>
+            
 
-            <input type="submit" value="Go to Venmo" />
+            <input className={styles.formButton} type="submit" value="Go to Venmo" />
         </form>
     );
 }
